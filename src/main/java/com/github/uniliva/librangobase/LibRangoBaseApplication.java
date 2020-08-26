a64f9eeb92e99ea7da07d12aab116918395bb2e5
Uniliva <uniliva.pereira@confitec.com.br>
2020-08-25 22:19:24 -0300

    Commit de esturutração do projeto

---
+++ src/main/java/com/github/uniliva/librangobase/LibRangoBaseApplication.java
@@ -0,0 +1,13 @@
+package com.github.uniliva.librangobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibRangoBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibRangoBaseApplication.class, args);
+	}
+
+}
